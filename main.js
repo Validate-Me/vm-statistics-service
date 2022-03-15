354f4d567ce917d209fc05a16f89a0b5404cad99
raman7070 <101165652+raman7070@users.noreply.github.com>
2022-03-15 14:18:08 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 const cron = require('node-cron');
-const main = require('./index')
+var spawn = require('child_process').spawn;
+var grep = spawn('grep', ['daily-email']);
+var ps = spawn('ps', ['a']);
+ps.stdout.pipe(grep.stdin);
+grep.stdout.on('data', function (data) {
+    var arr = data.toString("utf8").trim().split("\n")
+     a = arr.length
+    // console.log(a)
+    if (a === 1) {
+        const task = require('./schedule')
+        task.start()
+        
+    }
+    else{
+        console.log("this service is already running")
+    }
 
-cron.schedule('00 08 * * *', function() {
-  try{
-    main()
-  }catch(error){
-    console.log(error)
-  }
- 
-    
-  });
-  
+});
+
